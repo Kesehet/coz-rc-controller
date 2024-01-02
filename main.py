af40940c6193f5f8ca84b9051ef81391f4ab97c1
Kesehet <hamood.siddiqui@gmail.com>
2024-01-02 08:29:44 +0530

    test

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/myname/')
 def home():
-    return 'Hello, World!'
+    return "My Name is RC Number 1"
 
 if __name__ == '__main__':
     app.run(debug=True)
